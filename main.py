0f390048293c31240ced196eb6a2c6840743e718
Staninna <26458805+Staninna@users.noreply.github.com>
2023-02-07 23:38:03 +0100

    Formatted code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import lib_coasterimg as coasterimg
 import time
 import os
 
-#Read check values
+# Read check values
 file1 = open("rules/age.txt", "r")
 age_check = int(file1.read())
 file1.close()
@@ -14,27 +14,27 @@ file1.close()
 running = True
 while running:
 
-    #Get inputs
-    os.system('cls')
+    # Get inputs
+    os.system("cls")
     print("Rollercoaster-check™")
     age = input("Voer leeftijd in: ")
     height = input("Voer lengte in: ")
     age = int(age)
     height = int(height)
 
-    #Process checks
-    if(age > age_check and height > height_check):
-        os.system('cls')
+    # Process checks
+    if age > age_check and height > height_check:
+        os.system("cls")
         print("Stap maar in!")
         print(coasterimg.get())
         time.sleep(2)
 
     else:
-        os.system('cls')
+        os.system("cls")
         print("Je voldoet niet aan de voorwaarden...")
         print(coasterimg.sad())
         time.sleep(2)
 
     result = input("Druk op Enter om nog een keer te checken, of X om te stoppen\n\n")
-    if(result.upper() == "X"):
+    if result.upper() == "X":
         running = False
